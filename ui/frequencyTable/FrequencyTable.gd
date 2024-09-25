7c9801832fd34cf1e0dfe534466198aacdc2029f
Michael Lugo <michaelrlugo@gmail.com>
2024-09-25 09:34:18 -0400

    The Frequency Table groups by frequency and displays all qualifying days, sorted by calendar order.

--- ui/frequencyTable/FrequencyTable.gd
+++ ui/frequencyTable/FrequencyTable.gd
@@ -7,8 +7,23 @@ const freqRowTemplate = preload("res://ui/frequencyTable/FrequencyTableRow.tscn"
 func populate_days(days: Array[Day], counts: Dictionary) -> void:
 	for child in get_children():
 		child.queue_free()
-		
+	
+	var striated = Dictionary()
+	
 	for day in days:
+		var count = counts[day._to_string()]
+		
+		if not striated.has(count):
+			striated[count] = Dictionary()
+		
+		striated[count][day._to_string()] = day
+	
+	var sorted_keys = striated.keys()
+	
+	sorted_keys.sort()
+	sorted_keys.reverse()
+	
+	for count in sorted_keys:
 		var row = freqRowTemplate.instantiate()
-		row.populate(day, counts[day._to_string()])
+		row.populate(count, striated[count])
 		add_child(row)
--- ui/frequencyTable/FrequencyTableRow.gd
+++ ui/frequencyTable/FrequencyTableRow.gd
@@ -1,13 +1,29 @@
 extends PanelContainer
 
-@onready var label: Label = $HBoxContainer/Label
+@onready var label: Label = $VBoxContainer/Label
+@onready var daysContainer: HFlowContainer = $VBoxContainer/DaysContainer
 
-var day: Day = Day.new(Month.new("?", "?", 1, 1))
+var days: Dictionary = Dictionary()
 var frequency: int = 0
 
-func populate(p_day: Day, p_frequency: int):
-	day = p_day
+func populate(p_frequency: int, p_days: Dictionary):
+	days = p_days
 	frequency = p_frequency
 
 func _ready() -> void:
-	label.text = "%s has frequency %s" % [day._to_string(), frequency]
+	label.text = "Frequency %s" % [frequency]
+	
+	for child in daysContainer.get_children():
+		child.queue_free()
+	
+	var sorted_days: Array[Day] = []
+	for d in days.values():
+		sorted_days.append(d)
+	
+	sorted_days.sort_custom(func (x,y):
+		return CALENDAR.day_to_ordinal(x) < CALENDAR.day_to_ordinal(y)
+	)
+	for day in sorted_days:
+		var day_label = Label.new()
+		day_label.text = day._to_string()
+		daysContainer.add_child(day_label)
